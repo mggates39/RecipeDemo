049a0e749c4d3c8f8a0cf6105e9a58d0bec09a7e
Marshall G Gates <mggates@gmail.com>
2021-07-11 23:28:19 -0400

    Refactored inventory item model
    Added a parent item reference for things that can be split like eggs
    Added insert query
    Made table name pural

--- Model/InventoryItem.py
+++ Model/InventoryItem.py
@@ -1,16 +1,28 @@
 
 class InventoryItem:
-    create_sql: str = """CREATE TABLE inventory_item (
+    create_sql: str = """CREATE TABLE inventory_items (
         item_id   INTEGER NOT NULL PRIMARY KEY,
+        parent_item_id INTEGER REFERENCES inventory_item,
         name VARCHAR,
         description VARCHAR
-        )"""
+    )"""
 
-    def __init__(self, item_id, name, description) -> None:
+    insert_query = """insert into inventory_items ( 
+        name, 
+        description,
+        parent_item_id
+    ) values (
+        :name, 
+        :description,
+        :parent_item_id
+    )"""
+
+    def __init__(self, item_id, name, description, parent_item_id=None) -> None:
         super().__init__()
         self.__item_id = item_id
         self.__name = name
         self.__description = description
+        self.__parent_item_id = parent_item_id
 
     @property
     def item_id(self):
@@ -19,6 +31,14 @@ class InventoryItem:
     @item_id.setter
     def item_id(self, item_id):
         self.__item_id = item_id
+
+    @property
+    def parent_item_id(self):
+        return self.__parent_item_id
+
+    @parent_item_id.setter
+    def parent_item_id(self, parent_item_id):
+        self.__parent_item_id = parent_item_id
 
     @property
     def name(self):
